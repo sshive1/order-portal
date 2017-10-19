e231f1caa674fdfe1785318b6208e67cf06fe8dd
Samuel D. Shive <sshive@PDC.local>
2017-10-19 10:01:33 -0400

    Changes to reflect new functionality needs

--- imports/api/tasks.tests.js
+++ imports/api/tasks.tests.js
@@ -1,5 +1,5 @@
-import { Meteor } from 'meteor/meteor'
-import { Random } from 'meteor/random'
+import { Meteor } from 'meteor/meteor';
+import { Random } from 'meteor/random';
 import { assert } from 'meteor/practicalmeteor:chai';
 
 import { Tasks } from './tasks.js';
